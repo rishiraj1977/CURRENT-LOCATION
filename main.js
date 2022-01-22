f78f8a28bb2d51acc90752197b493b77a5f113c6
RISHIRAJ <58257428+rishiraj1977@users.noreply.github.com>
2022-01-22 20:41:03 +0530

    added files

---
+++ main.js
@@ -0,0 +1,68 @@
+let latitude, longitude, destination;
+
+$(document).ready(function () {
+    alert("Please allow the device to know your location!")
+    initGeolocation();
+})
+
+$(function () {
+    $("#navigate-button").click(function () {
+        window.location.href = `ar_navigation.html?source=${latitude};${longitude}&destination=${destination.lat};${destination.lng}`
+    })
+})
+
+function initGeolocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success);
+    }
+    else {
+        alert("Sorry, your browser does not support geolocation services.");
+    }
+}
+
+function success(position) {
+    longitude = position.coords.longitude;
+    latitude = position.coords.latitude
+
+    // Initializing Mapbox
+    mapboxgl.accessToken = 'pk.eyJ1IjoicmlzaGlyYWoxOTc3IiwiYSI6ImNreTZwaXh4NDB4d24ycG80ZDdoOXRzZnkifQ.cNYlGADu8wEHXrOBs1Y1tg';
+
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y',
+        center: [longitude, latitude],
+        zoom: 16
+    });
+
+    map.addControl(
+        new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true
+        })
+    );
+
+    map.addControl(
+        new MapboxDirections({
+            accessToken: mapboxgl.accessToken
+        }),
+        'top-left'
+    );
+
+    map.on('click', function (e) {
+        destination = e.lngLat;
+    });
+
+    map.addLayer({
+        'id': 'sky',
+        'type': 'sky',
+        'paint': {
+        'sky-type': 'atmosphere',
+        'sky-atmosphere-sun': [0.0, 0.0],
+        'sky-atmosphere-sun-intensity': 15
+        }
+        });
+        
+}
+
